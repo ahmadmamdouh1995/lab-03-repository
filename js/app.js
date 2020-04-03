096c44b1be8cc52e8a273b131966c47ebc262be8
ahmadmamdouh <mamdouh486@gmail.com>
2020-04-03 22:58:36 +0300

    add button

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 'use strict';
 let ArryKeys = [];
 let allHorns = [];
+let page1 = [];
+let page2 = [];
 $(document).ready(function () {
     function Horns(horn) {
         this.title = horn.title;
@@ -10,16 +12,11 @@ $(document).ready(function () {
         this.horns = horn.horns;
         allHorns.push(this);
     }
-  //0 new array for keywords
 
     Horns.prototype.render = function () {
-        let $hornClone = $("#photo-template").clone();
-        $hornClone.find("h2").text(`Name : ${this.title}`);
-        $hornClone.find("img").attr("src", this.image_url);
-        $hornClone.find('p').text(`Description :${this.description}`);
-        $hornClone.removeAttr('id');
-        $hornClone.attr('class', this.keyword);
-        $("main").append($hornClone);
+        let $hornClone = $("#photo-template").html();
+        var rendered = Mustache.render($hornClone , this);
+        $("main").append(rendered);
     };
     
     Horns.prototype.renderSelect =function() {
@@ -28,37 +25,42 @@ $(document).ready(function () {
             ArryKeys.push(this.keyword);
             selectMenu.append(`<option> ${this.keyword} </option>`);
         }
-        // get the select
-        // loop through all the keywords
-        // add it to the select Hint (.append()) TODO search for it
-    };
-    
-    const readJson = () => {
-        $.ajax("data/page-1.json", { method: "GET", dataType: "JSON" }).then(data => {
+   };
+
+       
+    const readJson = (num) => {
+        $.ajax(`data/page-${num}.json`, { method: "GET", dataType: "JSON" }).then(data => {
             data.forEach(hornItem => {
                 let horn = new Horns(hornItem);
                 horn.renderSelect();
                 horn.render();
-                 // if its not in the keyword array
-                 // added it there
             });
-            // renderSelect();
-        })
+       })
     };
-    readJson();
+    readJson(1);
+    readJson(2);
+
+
+    $('#p1').on('click', function(){
+              
+    });
+
+
+    $('#p2').on('click', function(){
+       
+    });
 });
+
         $('.sel').change(function() {
             let keys = $(this).children('option:selected').val();
             ArryKeys.forEach(function(val){
                 if(keys === val){
                     $('div').hide();
                     $(`.${val}`).show();
-                    console.log(5);
+                    
                 }
             });
-            // 1 get the value ===> keyword hint $(this)
-            // 2 hide the elements and only show the ones with id == keyword
-          });
+         });
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
